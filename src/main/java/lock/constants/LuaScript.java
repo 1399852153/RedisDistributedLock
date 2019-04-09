2eb65a37818467a07ff8ea624412e8c510524081
1399852153 <139985215.@qq.com>
2019-04-09 23:44:57 +0800

    实现可重入锁特性

--- src/main/java/lock/constants/LuaScript.java
+++ src/main/java/lock/constants/LuaScript.java
@@ -13,8 +13,14 @@ public class LuaScript {
             + "then return redis.call('del', KEYS[1]) "
             + "else return 0 end";
 
-    /**
-     * 可重入锁更新脚本
+    /**k
+     * 可重入锁更新脚本 todo
+     *
+     * 1. 判断key是否存在
+     * 2. 如果存在，判断requestID是否相等
+     *            相等，则删除掉key重新创建新的key值，重置过期时间
+     *            不相等，说明已经被抢占，加锁失败，返回null
+     * 3. 如果不存在，说明恰好已经过期，重新生成key
      * */
     public static final String RLOCK_SCRIPT = "";
 }
