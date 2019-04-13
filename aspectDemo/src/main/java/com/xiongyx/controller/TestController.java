6738083eac737ce6fb3be064f6ba5f053e16c930
xiongyx <1399852153@qq.com>
2019-04-13 15:25:08 +0800

    增加测试代码

--- aspectDemo/src/main/java/com/xiongyx/controller/TestController.java
+++ aspectDemo/src/main/java/com/xiongyx/controller/TestController.java
@@ -3,13 +3,17 @@ package com.xiongyx.controller;
 import com.xiongyx.service.api.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @Author xiongyx
  * on 2019/4/12.
  */
+@CrossOrigin
 @Controller
 public class TestController {
 
@@ -18,11 +22,15 @@ public class TestController {
 
     @RequestMapping("/test")
     @ResponseBody
-    public String test(){
+    public String test(HttpServletRequest request){
+        String num = request.getParameter("num");
+        System.out.println("接收到请求 num=" + num);
+
         testService.method1();
 
         testService.method2();
 
+        System.out.println("请求处理完毕 num=" + num);
         return "ok";
     }
 }
