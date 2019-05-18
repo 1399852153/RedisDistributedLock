84ea0dcb10c721ec4b2bd0fd456197f803708118
1399852153 <139985215.@qq.com>
2019-05-18 19:13:01 +0800

    redisTemplateDemo

--- redisTemplate/src/main/java/com/xiongyx/config/RedisConfig.java
+++ redisTemplate/src/main/java/com/xiongyx/config/RedisConfig.java
@@ -3,13 +3,13 @@ package com.xiongyx.config;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.cache.CacheManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -27,11 +27,12 @@ public class RedisConfig extends CachingConfigurerSupport {
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
+
         // 配置连接工厂
         template.setConnectionFactory(factory);
 
         //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
-        Jackson2JsonRedisSerializer jacksonSeial = new Jackson2JsonRedisSerializer(Object.class);
+        Jackson2JsonRedisSerializer jacksonSeial = new Jackson2JsonRedisSerializer<>(Object.class);
 
         ObjectMapper om = new ObjectMapper();
         // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
--- redisTemplate/src/main/java/com/xiongyx/controller/HelloController.java
+++ redisTemplate/src/main/java/com/xiongyx/controller/HelloController.java
@@ -2,7 +2,9 @@ package com.xiongyx.controller;
 
 import com.xiongyx.config.RedisConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,8 +17,19 @@ public class HelloController {
     @Autowired
     private RedisConfig redisConfig;
 
+    @Autowired
+    private RedisTemplate<String,Object> redisTemplate;
+
     @RequestMapping("/test")
-    public String hello(){
-        return redisConfig.toString();
+    public String hello(@RequestParam("id")String id){
+        String oldValue = (String)redisTemplate.opsForValue().get("userID");
+        System.out.println(oldValue);
+
+        redisTemplate.opsForValue().set("userID",id);
+
+        String newValue = (String)redisTemplate.opsForValue().get("userID");
+        System.out.println(newValue);
+
+        return newValue;
     }
 }
